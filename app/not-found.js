e180ad08d2e1dabc60e2a8d3bc75789c03ff2276
sumitbansode360 <sumitbansode360@gmail.com>
2025-07-25 17:21:36 +0530

    404 page fixed

--- app/not-found.js
+++ app/not-found.js
@@ -1,9 +1,10 @@
-// app/not-found.js
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="text-center mt-20 text-slate-700">
       <h1 className="text-4xl font-bold text-blue-600">404 – Page Not Found</h1>
-      <p className="mt-4">Let’s get you back <a className="text-blue-500 underline" href="/">home</a>.</p>
+      <p className="mt-4">Let’s get you back <Link href="/" className="text-blue-500 underline">home</Link>.</p>
     </div>
   );
 }
